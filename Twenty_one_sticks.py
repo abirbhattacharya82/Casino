f4c301c5eeaeb7738bedbf9542d037646f61e264
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2020-11-11 20:43:47 +0530

    Create Twenty_one_sticks.py

---
+++ Twenty_one_sticks.py
@@ -0,0 +1,13 @@
+print("WARNING: YOU WILL NEVER WIN THIS GAME")
+x=21
+while x>1:
+    x_i=int(input("Pick up 1-4 sticks "))
+    if x_i>4 or x_i<1:
+        print("Illegal You are Disquallified")
+    else:
+        x=x-5
+        y=(5-x_i)
+        print("I pick ",y," sticks")
+        print("Sticks Remaining: ",x)
+print("You Lost")
+print(":(")
