8b806b67e5291357b1a2d72344f28c71eef0c8f4
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2020-11-12 08:26:55 +0530

    Update Roll_the_correct_dice.py

--- Roll_the_correct_dice.py
+++ Roll_the_correct_dice.py
@@ -1,17 +1,25 @@
 import random
-k=int(input("Enter your Total Balance in the Account: "))
-bet=int(input("Place your Bets: "))
-if bet>k:
-    print("Unable to Place Bet")
-else:
-    j=int(input("Guess the Number on the Dice: "))
-    x=random.randint(1,6)
-    if x==j:
-        print("You Won")
-        bet=bet+bet
-        k=k+bet
-        print("Current Balance: ",k)
+print("you have 5000 rupees in your account")
+k=5000
+f=5
+while (True):
+    bet=int(input("Place your Bets: "))
+    if bet>k:
+        print("Unable to Place Bet")
     else:
-        print("You Lost")
-        k=k-bet
-        print("Current Balance: ",k)
+        j=int(input("Guess the Number on the Dice: "))
+        x=random.randint(1,6)
+        if x == j:
+            print("You Won")
+            bet=bet+bet
+            k = k+bet
+            print("Current Balance: ",k)
+        else:
+            print("You Lost")
+            k=k-bet
+            print("Current Balance: ",k)
+            f=f-1
+    if(k==0):
+        break
+    elif(f==0):
+        break
